9c9f04a5dd23732f5a933b97b30509a7c0fe9bbe
yjlee0235 <yjlee0235@naver.com>
2023-09-30 23:47:15 +0900

    Inital Commit

---
+++ src/main/java/me/lnsc/pathgenerator/PathgeneratorApplication.java
@@ -0,0 +1,13 @@
+package me.lnsc.pathgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/lnsc/pathgenerator/PathgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lnsc.pathgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
