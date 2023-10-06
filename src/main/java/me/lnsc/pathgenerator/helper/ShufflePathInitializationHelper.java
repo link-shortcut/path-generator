d50270a8c2657190b99a791dcbf2e3db5420e767
yjlee0235 <yjlee0235@naver.com>
2023-10-06 11:50:12 +0900

    Feat: Zookeeper를 통해 Path Generator 초기화를 도와주는 Helper 객체 추가

---
+++ src/main/java/me/lnsc/pathgenerator/helper/ShufflePathInitializationHelper.java
@@ -0,0 +1,28 @@
+package me.lnsc.pathgenerator.helper;
+
+import me.lnsc.pathgenerator.domain.ShuffleRandomPathGenerator;
+import me.lnsc.pathgenerator.service.ZookeeperService;
+import org.apache.zookeeper.KeeperException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+
+public class ShufflePathInitializationHelper {
+    private static final Logger log = LoggerFactory.getLogger(ShuffleRandomPathGenerator.class);
+    private final ZookeeperService zookeeperService;
+    private final ShuffleRandomPathGenerator pathGenerator;
+
+    public ShufflePathInitializationHelper(ZookeeperService zookeeperService,
+                                           ShuffleRandomPathGenerator pathGenerator) {
+        this.zookeeperService = zookeeperService;
+        this.pathGenerator = pathGenerator;
+    }
+
+    public void initializePathGenerator() throws UnsupportedEncodingException, InterruptedException, KeeperException {
+        int gap = pathGenerator.getGap();
+        long offset = zookeeperService.increaseOffset(gap);
+        pathGenerator.initialize(offset);
+        log.info("Shuffle path generator is initialized");
+    }
+}
